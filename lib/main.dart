a7d76f10a48bc1b2ebb4f4b3e6d5d6186e7b391c
Horaunt <pranaypandey22254518@gmail.com>
2023-10-21 23:56:50 +0530

    Simple changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,94 +2,110 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Profilecard(),
+    home: ProfileCard(),
   ));
 }
 
-class Profilecard extends StatelessWidget {
-  const Profilecard({super.key});
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black87,
       appBar: AppBar(
         title: const Text('Profile Card'),
         centerTitle: true,
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.black87,
       ),
-      body:  Padding(
+      body: Padding(
         padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-           const Center(
-            child:CircleAvatar(
-              backgroundImage: NetworkImage('https://yt3.ggpht.com/ytc/APkrFKY9CpuT_Z3puHuxk7YgPGr3Mjf-lx_G6QkpnLbItA=s600-c-k-c0x00ffffff-no-rj-rp-mo'),
-              radius: 80.0,
-            ),),
-            // const SizedBox(width: 10.0),
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://img.freepik.com/premium-vector/3d-avatar_665280-46952.jpg'),
+                radius: 80.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
             const Divider(
               height: 100.0,
-              color: Colors.white,
+              color: Colors.deepPurpleAccent,
             ),
             const Text(
               'Name: ',
               style: TextStyle(
-                color: Colors.white,
+                color: Color.fromRGBO(240	,101	,67, 60),
                 letterSpacing: 2.0,
               ),
             ),
-            const SizedBox(height: 10.0,),
+            const SizedBox(height: 10.0),
             const Text(
               'Pranay Pandey',
               style: TextStyle(
-                color: Colors.amberAccent,
-                letterSpacing: 2.0,
-              fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 30.0,),
-            const Text(
-              'Profession: ',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(height: 10.0,),
-            const Text(
-              'Student',
-              style: TextStyle(
-                color: Colors.amberAccent,
+                color: Color.fromRGBO(199, 249, 204, 100),
                 letterSpacing: 2.0,
                 fontSize: 28.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 30.0,),
+            const SizedBox(height: 30.0),
+            const Text(
+              'Profession: ',
+              style: TextStyle(
+                color: Color.fromRGBO(240	,101	,67, 60),
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const Text(
+              'Student',
+              style: TextStyle(
+                color: Color.fromRGBO(199, 249, 204, 100),
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30.0),
             Row(
               children: <Widget>[
                 const Icon(
                   Icons.email,
-                  color:Colors.grey,
+                  color: Color.fromRGBO(240	,101	,67, 60),
                 ),
                 const SizedBox(width: 10.0),
                 Text(
                   'pranaypandey22254518@gmail.com',
                   style: TextStyle(
                     color: Colors.grey[400],
-                    fontSize: 18.0,
+                    fontSize: 15.0,
                     letterSpacing: 0.1,
                   ),
-                )
+                ),
               ],
-            )
-          ]
-      )
-      )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+class Test extends StatefulWidget {
+  const Test({super.key});
 
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  int counter = 1;
+
+  @override
+
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
